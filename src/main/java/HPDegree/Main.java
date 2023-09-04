07f5b3e6d87aacd69271a167ff9f9e7855657b74
Yon <yonkwon90@gmail.com>
2023-09-04 12:30:58 -0500

    [Fix] Filename for Beta run & Theta run

--- src/main/java/HPDegree/Main.java
+++ src/main/java/HPDegree/Main.java
@@ -13,7 +13,7 @@ public class Main {
   static final double WEIGHT_ON_CHARACTERISTIC = 1D;
   static final double WEIGHT_ON_BELIEF = 1D - WEIGHT_ON_CHARACTERISTIC;
 
-  static final int ITERATION = 5_000;
+  static final int ITERATION = 1_000;
   static final int TIME = 500 + 1;
 
   //  static final int N_OF_UNIT = 5;
@@ -25,7 +25,7 @@ public class Main {
   static final int N = N_OF_UNIT * N_IN_UNIT;
   static final int DENSITY = N_OF_UNIT * N_IN_UNIT * (N_IN_UNIT - 1) / 2;
 
-  static final int OBSERVATION_SCOPE = 2; // >= 2
+  static final int OBSERVATION_SCOPE = 4; // >= 2
 
   static final int L = 1; // Fixed param
   static final int M_OF_BUNDLE = 20;
@@ -40,28 +40,28 @@ public class Main {
   //  static final double[] THETA = {.01, .125, .25, .375, .5, .625, .75, .875, .99};
   static final double[] THETA = {.0001, 0.0625, 0.125, 0.1875, 0.25, 0.3125, 0.375, 0.4375, 0.5, 0.5625, 0.625, 0.6875, 0.75, 0.8125, 0.875, 0.9375, .9999};
   //  static final double[] THETA = {.0001, 0.0625, 0.125, 0.1875, 0.25, 0.3125, 0.375, 0.4375, 0.5, .75, .9999};
-  static final int GRANULARITY_THETA = THETA.length;
+  static final int LENGTH_THETA = THETA.length;
 
   static final int ITERATION_BETA = 100_000;
   //  static final double[] BETA_CANDIDATE = {.06, .061, .062, .063, .064, .065, .066, .067};
   static final double[] BETA_CANDIDATE = {.086, .087, .088, .089, .09, .091, .092, .093, .094, .095, .097};
-  static final int GRANULARITY_BETA_CANDIDATE = BETA_CANDIDATE.length;
+  static final int LENGTH_BETA_CANDIDATE = BETA_CANDIDATE.length;
   static double OPTIMAL_BETA = .090; // 01... Set by 100_000 iterations
   static double OPTIMAL_BETA_LEFT = 1D - OPTIMAL_BETA; // Set by 10000 iterations
 
 
-  //  static final double[] BETA = {0, 1};
+  static final double[] BETA = {0, 1};
 //  static final double[] BETA = {0, .25, .5, .75, 1.0};
 //  static final double[] BETA = {0, .125, .25, .375, .5, .625, .75, .875, 1.0};
-  static final double[] BETA = {0, 0.0625, 0.125, 0.1875, 0.25, 0.3125, 0.375, 0.4375, 0.5, 0.5625, 0.625, 0.6875, 0.75, 0.8125, 0.875, 0.9375, 1};
+//  static final double[] BETA = {0, 0.0625, 0.125, 0.1875, 0.25, 0.3125, 0.375, 0.4375, 0.5, 0.5625, 0.625, 0.6875, 0.75, 0.8125, 0.875, 0.9375, 1};
   static final int LENGTH_BETA = BETA.length;
 
   //  static final double[] E = {.25};
-  static final double[] E = {0, .25};
+  static final double[] E = {0, .25, .75};
   static final int LENGTH_E = E.length;
 
-    static final double[] A = {0};
-//  static final double[] A = {0, .25, .5, .75, 1};
+  static final double[] A = {0};
+  //  static final double[] A = {0, .25, .5, .75, 1};
   static final int LENGTH_A = A.length;
 
   static final double[] H = {0, 1};
@@ -70,7 +70,7 @@ public class Main {
 
   static final double P_LEARNING = .25;
 
-  static final int[] RESULT_KEY_VALUE = {LENGTH_H, GRANULARITY_THETA, LENGTH_E, LENGTH_A, TIME};
+  static final int[] RESULT_KEY_VALUE = {LENGTH_H, LENGTH_THETA, LENGTH_E, LENGTH_A, TIME};
 
   static final int[] RESULT_KEY_VALUE_DECOMPOSITION = {LENGTH_H, LENGTH_BETA, LENGTH_E, LENGTH_A, TIME};
 
@@ -86,7 +86,7 @@ public class Main {
           "W" + WEIGHT_ON_CHARACTERISTIC +
           "M" + M_OF_BUNDLE + "X" + M_IN_BUNDLE +
           "H" + LENGTH_H +
-          "Th" + GRANULARITY_THETA +
+          "Th" + LENGTH_THETA +
           "(Opt" + OPTIMAL_BETA + (GET_OPTIMAL_BETA ? "Calc" : "Set") + ")" +
           "E" + LENGTH_E +
           "A" + LENGTH_A +
@@ -154,8 +154,7 @@ public class Main {
             "W" + WEIGHT_ON_CHARACTERISTIC +
             "M" + M_OF_BUNDLE + "X" + M_IN_BUNDLE +
             "H" + LENGTH_H +
-            "Th" + GRANULARITY_THETA +
-            "(Opt" + OPTIMAL_BETA + (GET_OPTIMAL_BETA ? "Calc" : "Set") + ")" +
+            "B" + LENGTH_BETA +
             "E" + LENGTH_E +
             "A" + LENGTH_A +
             "P" + P_LEARNING :
@@ -170,7 +169,8 @@ public class Main {
             "M" + M_OF_BUNDLE +
             "X" + M_IN_BUNDLE +
             "H" + LENGTH_H +
-            "B" + LENGTH_BETA +
+            "Th" + LENGTH_THETA +
+            "(Opt" + OPTIMAL_BETA + (GET_OPTIMAL_BETA ? "Calc" : "Set") + ")" +
             "E" + LENGTH_E +
             "A" + LENGTH_A +
             "P" + P_LEARNING);
