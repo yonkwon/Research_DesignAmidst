58402e3512034b0e5495716e1b55235223b97bff
Yon <yonkwon90@gmail.com>
2024-09-11 00:32:29 -0500

    [Add.] Variant with positional rewiring w/ probabilistic individual choice

---
+++ src/main/java/DAPositionalRewiring/Main.java
@@ -0,0 +1,82 @@
+package DAPositionalRewiring;
+
+import java.io.File;
+
+public class Main {
+
+  static String RUN_ID = "DAPosition";
+
+  static final long TIC = System.currentTimeMillis();
+
+  static final boolean GET_GRAPH = true;
+  static final boolean GET_MAT = true;
+  static final boolean LINK_LEVEL = false;
+  static final double LINK_ADD = .00;
+  static final boolean DO_POST_REWIRING = true;
+
+//  static final int ITERATION = 10_000;
+  static final int ITERATION = 1000;
+  static final int TIME = 600 + 1;
+
+  static final int NUM_MECHANISM = 2; // Closure & Preferential attachment
+
+  static final int N = 100;
+  static final int[] SPAN = {2, 3, 4, 5, 6, 7, 8};
+//  static final int[] SPAN = {2, 5, 8};
+  static final int LENGTH_SPAN = SPAN.length;
+
+  static final int N_DYAD_INT = N * (N - 1) / 2;
+  static final double N_DYAD = N * (N - 1D) / 2D;
+  static final double INFORMAL_INITIAL_PROP = .05; // >= INFORMAL_TURNOVER
+  static final double INFORMAL_TURNOVER_PROP = .02;
+  static final int INFORMAL_INITIAL_NUM = (int) (N_DYAD_INT * INFORMAL_INITIAL_PROP); // >= INFORMAL_TURNOVER
+  static final int INFORMAL_TURNOVER_NUM = (int) (N_DYAD_INT * INFORMAL_TURNOVER_PROP);
+  static final int MAX_INFORMAL = 15;
+
+  static final int M_OF_BUNDLE = 20;
+  static final int M_IN_BUNDLE = 5;
+  static final int M = M_OF_BUNDLE * M_IN_BUNDLE;
+  static final double M_N = M * N;
+  static final double M_N_DYAD = M * N_DYAD;
+
+  static final double[] ENFORCEMENT = {1};
+//  static final double[] ENFORCEMENT = {0, 1};
+  static final int LENGTH_ENFORCEMENT = ENFORCEMENT.length;
+  static final double P_LEARNING = .2;
+
+  //Parameters of Hompohily
+  static final int[] RESULT_KEY_VALUE = {NUM_MECHANISM, LENGTH_SPAN, LENGTH_ENFORCEMENT, TIME};
+
+  static String FILENAME;
+  static String PATH_CSV;
+
+  public static void main(String[] args) {
+    setFileName();
+    System.out.println("Target File: " + FILENAME);
+    Computation c = new Computation();
+    if (GET_GRAPH) {
+      c.printNetwork();
+    }
+    if (GET_MAT) {
+      c.doExperiment();
+      new MatWriter(c);
+    }
+    System.out.println("Finished: " + FILENAME);
+  }
+
+  private static void setFileName() {
+    FILENAME = RUN_ID +
+        "I" + ITERATION +
+        "_LL" + (LINK_LEVEL ? "t" : "f") +
+        "_LA" + LINK_ADD +
+        "_PL" +
+        (DO_POST_REWIRING? "t("+INFORMAL_INITIAL_PROP+"&"+INFORMAL_TURNOVER_PROP+")" : "f") +
+        "_T" + TIME +
+        "_N" + N +
+        "_M" + M_OF_BUNDLE + "X" + M_IN_BUNDLE +
+        "_S" + LENGTH_SPAN +
+        "_E" + LENGTH_ENFORCEMENT +
+        "_P" + P_LEARNING;
+    PATH_CSV = new File(".").getAbsolutePath() + "\\" + FILENAME + "\\";
+  }
+}
